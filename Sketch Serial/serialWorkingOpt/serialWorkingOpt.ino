8ad79f25c7e8fd7ad1fc6b95bd1a961b16973a59
Ceriel Boosveld <ceriel.boosveld@gmail.com>
2014-12-23 23:15:11 +0100

    Sketchupdate

---
+++ Sketch Serial/serialWorkingOpt/serialWorkingOpt.ino
@@ -0,0 +1,30 @@
+#include <AltSoftSerial.h>
+// AltSoftSerial always uses these pins:
+//
+// Board          Transmit  Receive   PWM Unusable
+// -----          --------  -------   ------------
+// Teensy 2.0         9        10       (none)
+// Teensy++ 2.0      25         4       26, 27
+// Arduino Uno        9         8         10
+// Arduino Mega      46        48       44, 45
+// Wiring-S           5         6          4
+// Sanguino          13        14         12
+AltSoftSerial altSerial;
+char c;
+void setup() {
+  Serial.begin(9600);
+  altSerial.begin(9600);
+}
+void loop() {
+  if (altSerial.available()) {
+    c = altSerial.read();
+
+    // --- 7 bits instelling ---
+    c &= ~(1 << 7);
+    
+    char inChar = (char)c;
+
+    Serial.print(inChar);
+  }
+}
+
